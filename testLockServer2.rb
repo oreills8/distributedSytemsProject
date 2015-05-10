5e5a32036c62579e61c25bd3a9217aca31da4079
oreills8 <oreills8@tcd.ie>
2015-05-10 23:28:50 +0100

    Create testLockServer2.rb

---
+++ testLockServer2.rb
@@ -0,0 +1,30 @@
+require 'socket'      # Sockets are in standard library
+
+hostname = 'localhost'
+port = 2700
+IPAddress = Socket::getaddrinfo(Socket.gethostname,"echo",Socket::AF_INET)[0][3]
+chatroom = "distributedSystems"
+clientName = "Steven"
+
+    
+s = TCPSocket.open(hostname, port)
+
+
+
+s.puts "CHATROOM: #{2}\nJOIN_ID: #{2}\nLOCK: file1\n"
+#sleep(200);
+#s.puts "CHATROOM: #{1}\nJOIN_ID: #{1}\nLOCK: file1\n"
+#s.puts "CHATROOM: #{2}\nJOIN_ID: #{2}\nUNLOCK: file1\n"
+#s.puts "CHATROOM: #{1}\nJOIN_ID: #{1}\nUNLOCK: file2\n"
+ 
+#s.puts "CHATROOM: #{1}\nJOIN_ID: #{1}\nAVAILABLE_FILES\n"
+
+
+
+begin
+  while line = s.gets   # Read lines from the socket
+    print line
+  end 
+end until line == "Closing the connection." 
+puts "Connection Lost"     
+s.close               # Close the socket when done
